be858167dccf2fd84d0c61ffd8650b681b50b5dd
Nan DENG <nan.deng@oledcomm.com>
2015-05-13 17:16:53 +0200

    Implement BubbleSort and QuickSort

--- app/src/main/java/dengn/sortingexercise/Sort/BubbleSort.java
+++ app/src/main/java/dengn/sortingexercise/Sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package dengn.sortingexercise.Sort;
 
+import android.os.SystemClock;
+
 import dengn.sortingexercise.models.SortResult;
 
 /**
@@ -7,10 +9,21 @@ import dengn.sortingexercise.models.SortResult;
  */
 public class BubbleSort {
 
-    public static final SortResult BubbleSort(int[] randomArray){
+    public static final SortResult BubbleSort(int[] randomArray) {
+        long startTime = SystemClock.elapsedRealtimeNanos();
 
+        for(int i=0;i<randomArray.length;i++){
+            for(int j=0;j<randomArray.length-1-i;j++){
+                if(randomArray[j]>randomArray[j+1]){
+                    int temp = randomArray[j+1];
+                    randomArray[j+1] = randomArray[j];
+                    randomArray[j] = temp;
+                }
+            }
+        }
 
+        long endTime = SystemClock.elapsedRealtimeNanos();
 
-        return new SortResult(randomArray, 0);
+        return new SortResult(randomArray, endTime - startTime);
     }
 }
--- app/src/main/java/dengn/sortingexercise/Sort/QuickSort.java
+++ app/src/main/java/dengn/sortingexercise/Sort/QuickSort.java
@@ -1,5 +1,7 @@
 package dengn.sortingexercise.Sort;
 
+import android.os.SystemClock;
+
 import dengn.sortingexercise.models.SortResult;
 
 /**
@@ -9,6 +11,37 @@ public class QuickSort {
 
     public static final SortResult QuickSort(int[] randomArray){
 
-        return new SortResult(randomArray, 0);
+        long startTime = SystemClock.elapsedRealtimeNanos();
+
+        qsort(randomArray, 0, randomArray.length-1);
+
+        long endTime = SystemClock.elapsedRealtimeNanos();
+
+        return new SortResult(randomArray, endTime-startTime);
+    }
+
+    private static void qsort(int[] arr, int low, int high){
+        if (low < high){
+            int pivot=partition(arr, low, high);        //split the array to two parts
+            qsort(arr, low, pivot-1);                   //recursion sort of left sub array
+            qsort(arr, pivot+1, high);                  //recursion sort of right sub array
+        }
+    }
+    private static int partition(int[] arr, int low, int high){
+        int pivot = arr[low];     //pivot record
+        while (low<high){
+            while (low<high && arr[high]>=pivot) {
+                --high;
+            }
+            arr[low]=arr[high];             //exchange values smaller than pivot to left
+            while (low<high && arr[low]<=pivot) {
+                ++low;
+            }
+            arr[high] = arr[low];           //exchange values higher than pivot to right
+        }
+
+        arr[low] = pivot;
+
+        return low;
     }
 }
